a37e8311a94f223ac2bd629de22dcdc6e013adc1
Vishal Vunnam <vivu9928@colorado.edu>
2025-04-16 17:52:26 -0600

    how about this

--- src/pyyc/front_end/flatten.py
+++ src/pyyc/front_end/flatten.py
@@ -90,7 +90,7 @@ def heapify(ast_tree, free_vars):
             return node  
 
         def visit_Assign(self, node):
-            # self.generic_visit(node)
+            self.generic_visit(node.value)
             if isinstance(node.targets[0], ast.Name) and node.targets[0].id in self.free_vars:
                 node.targets[0] = ast.Subscript(
                         value=node.targets[0],  
